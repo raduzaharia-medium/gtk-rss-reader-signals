86078e7446351cad524c718e6942f26f57323e60
Radu Zaharia <101859038+raduzaharia-medium@users.noreply.github.com>
2022-03-31 17:51:43 +0300

    Create mod.rs

---
+++ src/feed_item/mod.rs
@@ -0,0 +1,14 @@
+mod template;
+
+use glib::{wrapper, Object};
+use template::FeedItemTemplate;
+
+wrapper! {
+    pub struct FeedItem(ObjectSubclass<FeedItemTemplate>);
+}
+
+impl FeedItem {
+    pub fn new(name: &str, url: &str) -> Self {
+        Object::new(&[("name", &name), ("url", &url)]).expect("Failed to create `FeedItem`.")
+    }
+}
